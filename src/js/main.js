ecf1c0238ecae53e98243c0b69db70acba87fa81
MrBandaruk <nbandaruk@gmail.com>
2018-06-05 02:32:24 +0300

    Landing works. Add js files, and images. Update css.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
-
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
